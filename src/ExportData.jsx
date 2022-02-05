f22aad608d530dccd4df04de34ed35981ecec280
AmitSaar <amit30060@gmial.com>
2022-02-05 19:38:09 +0200

    connected googlesheet

--- src/ExportData.jsx
+++ src/ExportData.jsx
@@ -1,14 +1,37 @@
 import React, { Component } from "react";
 import {render} from "react-dom"
+import axios from 'axios';
+
 
 class ExportData extends Component{
     exportToExcel = () => console.log("hi");
 
+    state = {
+        name: ''
+     }
+
+     changeHandler = (e) => {
+        this.setState({[e.target.name] : e.target.value})
+      }
+
+    submitHandler = e => {
+        e.preventDefault();
+        console.log(this.state);
+    
+        axios.post('https://sheet.best/api/sheets/dfbe7b03-f9ef-45a2-aa05-82600f0494b4', this.state)
+        .then(response => {
+          console.log(response);
+        })
+      }
+
 
     render(){
+        const {name}=this.state;
         return (
         <div>
-            <button onClick={this.exportToExcel}>Let's do it!</button>
+            <label>Name</label>
+            <input placeholder='Enter your name' type="text" name = "name" value = {name} onChange={this.changeHandler}/>
+            <button onClick={this.submitHandler}>Send</button>
         </div>
         )
     }
